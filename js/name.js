66c5245a4880d4d7e6fe1975056fff501eaeeacb
Houyu0926 <64799132+Houyu0926@users.noreply.github.com>
2021-02-16 11:19:40 +0100

    last

--- js/name.js
+++ js/name.js
@@ -5,7 +5,7 @@ var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula"
 
 for (var i = 0; i < names.length; i++) {
 	if (names[i].startsWith("J") || names[i].startsWith("j")){
-		console.log("Goodbye " + names[i]);
+		console.log("Good Bye " + names[i]);
 	}else{
 		console.log("Hello " + names[i]);
 	}
